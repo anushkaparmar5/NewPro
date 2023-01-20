2148ae2e1701b511e94484614a667d5fa7e27ca9
anushkaparmar5 <anushkaparmar@gmail.com>
2023-01-20 12:33:06 +0530

    Initial version of newpro generated by generator-jhipster@7.9.3

---
+++ src/main/java/comcompanymyapp/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package comcompanymyapp.repository;
+
+import comcompanymyapp.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
---
+++ src/main/java/comcompanymyapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package comcompanymyapp.service.dto;
